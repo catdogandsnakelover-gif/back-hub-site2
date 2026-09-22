1b4d4d3e8de5612c0e11087ae8cf18355981013e
catdogandsnakelover-gif <catdogandsnakelover@gmail.com>
2026-09-22 14:36:53 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use(express.json({ limit: "100kb" }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // ---------------- database ----------------
+require("fs").mkdirSync(path.dirname(DB_PATH), { recursive: true });
 const db = new Database(DB_PATH);
 db.pragma("journal_mode = WAL");
 db.exec(`
